108e6d8f9de6ac9616c9d455c14916101e5e52cd
matt <bronx0807@gmail.com>
2020-04-19 19:00:21 +0800

    add swagger ui

--- src/main/java/com/abc/crm/config/security/CrmWebMvcConfigurer.java
+++ src/main/java/com/abc/crm/config/security/CrmWebMvcConfigurer.java
@@ -1,6 +1,7 @@
 package com.abc.crm.config.security;
 
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 public class CrmWebMvcConfigurer implements WebMvcConfigurer {
@@ -8,5 +9,13 @@ public class CrmWebMvcConfigurer implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // http://localhost:8080/crm/swagger-ui.html
+        registry.addResourceHandler("/statics/**").addResourceLocations("classpath:/statics/");
+        registry.addResourceHandler("/swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 }
